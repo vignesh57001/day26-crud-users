063ae9ad6ab57095b24811a37564cdda8ea5bdf2
vignesh57001 <vigneshmenon2303@gmail.com>
2022-10-27 17:41:14 +0530

    new file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import SideBar from './SideBar';
+import TopBar from './TopBar';
+import DashBoard from './DashBoard';
+import Create_Users from './Create_Users';
+import View_User from './View_User';
+import Users from './Users';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./fontawesome-free/css/all.min.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div id="wrapper">
+          <SideBar />
+          <div id="content-wrapper" className="d-flex flex-column">
+            <div id="content">
+              <TopBar  />
+              
+              <Routes>
+                <Route path='/DashBoard' element={<DashBoard />}></Route>
+                <Route path='/Users' element={<Users />}></Route>
+                <Route path='/Create_Users' element={<Create_Users />}></Route>
+                <Route path='/user/:id' element={<View_User />}></Route>
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
